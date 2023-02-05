34f9058414a0a32aacff42d56534b078d9765e0c
Sebastian Luna <eliaslunaslo@gmail.com>
2023-02-05 10:11:12 -0300

    progress Bar finalized

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,16 +1,31 @@
-let point1 = document.getElementById("point1");
-let point2 = document.getElementById("point2");
-let point3 = document.getElementById("point3");
-let point4 = document.getElementById("point4");
-
-
-let bar = document.getElementById("bar2");
-
 let n = 0;
-
-function onClickNext() {
-
-    bar[n].classList.toggle("active");
-
-    n++;
-}
+document.getElementById("btn--next").addEventListener("click", function() {
+    let point = document.getElementsByClassName("point")[n];
+    point.classList.add("active");
+    let progressBar = document.getElementById("bar2");
+  
+  
+    if (n < 4) {
+      if(n<3){
+         progressBar.style.width = (n+1)*25 + "%"; // Actualiza la barra de progreso
+         progressBar.style.transition = "1s";
+        }
+       
+        n++;
+    }
+  });
+  
+  document.getElementById("btn--back").addEventListener("click", function() {
+        let point = document.getElementsByClassName("point")[n-1];
+    point.classList.remove("active");
+  
+    if (n > 0) {
+        n--;
+  
+    let progressBar = document.getElementById("bar2");
+    progressBar.style.width = n*25 + "%"; // Actualiza la barra de progreso
+    progressBar.style.transition = "1s";
+    }
+  });
+  
+  
