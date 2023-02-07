cda8709d71a6eb9b03286fec0e45b265e45deca8
Sebastian Luna <eliaslunaslo@gmail.com>
2023-02-06 22:31:11 -0300

    Day 4, Hidden Search Widget

---
+++ search-widget/script.js
@@ -0,0 +1,7 @@
+document.getElementById("img").addEventListener("click", function () {
+    let container = document.getElementById("search");
+
+    container.classList.toggle("width");
+    container.style.transition = "1s";
+
+})
