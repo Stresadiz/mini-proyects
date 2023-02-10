3f6d1da91da256df750e4ff47ff12628f71befff
Sebastian Luna <eliaslunaslo@gmail.com>
2023-02-10 16:02:53 -0300

    mobile form, wave style

---
+++ form-wave/script.js
@@ -0,0 +1,16 @@
+
+function wave() {
+
+    let title = document.getElementsByClassName("float")[0];
+
+    title.classList.add("float1");
+    title.style.transition = "0.5s";
+}
+
+function wave2() {
+
+    let title = document.getElementsByClassName("float")[1];
+
+    title.classList.add("float1");
+    title.style.transition = "0.5s";
+}
