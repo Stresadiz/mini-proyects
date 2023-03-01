f20f442128b822e455b9d1cadc01c3f80a9e116d
Sebastian Luna <eliaslunaslo@gmail.com>
2023-03-01 20:30:02 -0300

    Upload new index and fixes

---
+++ movie-app/script.js
@@ -0,0 +1,7 @@
+document.getElementById("img").addEventListener("click", function () {
+    let search = document.getElementById("search");
+
+    search.classList.toggle("width--var");
+    search.style.transition = "1s";
+
+})
